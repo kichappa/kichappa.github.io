099418de399bf7256304bb7f359a8cc2264a67cf
kichappa <kichappa@gmail.com>
2022-12-06 22:49:25 +0000

    let there be light

---
+++ src/App.js
@@ -0,0 +1,17 @@
+import logo from './logo.svg';
+import './style.css'
+
+function App() {
+  return (
+    <div className="App">
+      <div className="menu">
+        <p>About</p>
+        <p>Projects</p>
+        <p>Publications</p>
+        <p>Questions</p>
+      </div>
+    </div>
+  );
+}
+
+export default App;
