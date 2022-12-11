8960de889fbddf87d0c0bacc686c9bd0e49866cb
Kishore S. Shenoy <kichappa@gmail.com>
2022-12-12 00:11:44 +0530

    Added Triangles

--- src/components/Projects.js
+++ src/components/Projects.js
@@ -15,6 +15,16 @@ import amp_plan3 from '../images/AMP Relax/AMP-Plan3.png';
 import amp_section from '../images/AMP Relax/AMP-Section.png';
 import draggy_point1 from '../images/Draggy/draggy-point1.png';
 import draggy_point2 from '../images/Draggy/draggy-point2.png';
+import tri_1 from '../images/Triangles/tri-1.svg';
+import tri_2 from '../images/Triangles/tri-2.svg';
+import tri_3 from '../images/Triangles/tri-3.svg';
+import tri_4 from '../images/Triangles/tri-4.svg';
+import tri_5 from '../images/Triangles/tri-5.svg';
+import tri_6 from '../images/Triangles/tri-6.svg';
+import tri_7 from '../images/Triangles/tri-7.svg';
+import tri_8 from '../images/Triangles/tri-8.svg';
+import tri_9 from '../images/Triangles/tri-9.svg';
+import tri_10 from '../images/Triangles/tri-10.svg';
 import Article from './Article';
 import { makeAuthors, makeLink } from './Article';
 
@@ -219,12 +229,12 @@ const Projects = ({ mobile, portrait, pathName }) => {
           {
             type: 'big-picture',
             images: [
+              [amp_plan1, 'Plan of ground floor'],
+              [amp_section, 'Sectional view of AMP'],
               [amp_birdseye, "A Bird's Eye view of AMP (blue)"],
               [amp_outside_r, 'View of AMP from its main entrance'],
               [amp_ff_oat_r, 'Open Air Theatre in the core'],
               //   [amp_designevolution, 'Evolution of design idea'],
-              [amp_section, 'Sectional view of AMP'],
-              [amp_plan1, 'Plan of ground floor'],
               //   [amp_plan2, 'Plan of first floor'],
               //   [amp_plan3, 'Plan of second floor'],
               //   [amp_ff_stairs_r, 'Stairs connecting first and second floor'],
@@ -294,6 +304,14 @@ const Projects = ({ mobile, portrait, pathName }) => {
                 [],
                 'Open PDF'
               )}
+              <br />
+              <br />
+              {makeLink(
+                'Wanna see a live render of AMP?',
+                'https://api2.enscape3d.com/v1/view/7e245c5c-875a-48e6-9e03-7bbc70ac8995',
+                [],
+                'Open website'
+              )}
             </p>
           </>
         }
@@ -359,6 +377,77 @@ const Projects = ({ mobile, portrait, pathName }) => {
                 'https://draggy.ramdon.team/?points=W3sicmFkaXVzIjowLCJjb2xvdXIiOnsiaHNsIjp7ImgiOjE5OS4xMzA0MzQ3ODI2MDg3LCJzIjowLjY4MDY3MjI2ODkwNzU2MzIsImwiOjAuMTI2OTEzNDk5OTk5OTk5OTgsImEiOjF9LCJoZXgiOiIjMGEyODM2IiwicmdiIjp7InIiOjEwLCJnIjo0MCwiYiI6NTQsImEiOjF9LCJoc3YiOnsiaCI6MTk5LjEzMDQzNDc4MjYwODcsInMiOjAuODEsInYiOjAuMjEzMywiYSI6MX0sIm9sZEh1ZSI6MTk5LjEzMDQzNDc4MjYwODcsInNvdXJjZSI6ImhzdiJ9LCJzaG93UGlja2VyIjpmYWxzZSwiY3VycmVudFhZIjp7IngiOjgxNCwieSI6NzI5fSwic2l6ZSI6WzI2LDI2XSwiY29udGFpbmVyU2l6ZSI6WzM2LDM2XX0seyJyZWYiOm51bGwsInJhZGl1cyI6MCwiY29sb3VyIjp7ImhzbCI6eyJoIjo2MCwicyI6MCwibCI6MSwiYSI6MX0sImhleCI6IiNmZmZmZmYiLCJyZ2IiOnsiciI6MjU1LCJnIjoyNTUsImIiOjI1NSwiYSI6MX0sImhzdiI6eyJoIjo2MCwicyI6MCwidiI6MSwiYSI6MX0sIm9sZEh1ZSI6NjAsInNvdXJjZSI6ImhzdiJ9LCJzaG93UGlja2VyIjpmYWxzZSwicG9pbnRlck9mZnNldCI6eyJ4IjowLCJ5IjowfSwiY3VycmVudFhZIjp7IngiOjIxODgsInkiOjk5Mn0sInNpemUiOlsyNiwyNl0sImNvbnRhaW5lclNpemUiOlszNiwzNl19LHsicmVmIjpudWxsLCJyYWRpdXMiOjQuMDY4ODgzNzA3NDk3MjY3NSwiY29sb3VyIjp7InJnYiI6eyJyIjoxMjEuMDU4NjM1MzM2MTE1NDMsImciOjgwLjk1MjYzNzMwNTkzNjE2LCJiIjoxOTUuNDE4NjE3MjQzODgwMSwiYSI6MX0sImhzdiI6eyJoIjoyNjEuMDIyNDg5Njc4NzI2NjMsInMiOjAuNTg1NzQ3NTY4NzQ0MTM3NSwidiI6MC43NjYzNDc1MTg2MDM0NTE0LCJhIjoxfSwiaHNsIjp7ImgiOjI2MS4wMjI0ODk2Nzg3MjY2MywicyI6MC40ODk5NDgxODU2MDI2NjMxLCJsIjowLjU0MTkwNDQyMDY4NTkxNDMsImEiOjF9LCJoZXgiOiIjNzk1MGMzIn0sInNob3dQaWNrZXIiOmZhbHNlLCJwb2ludGVyT2Zmc2V0Ijp7IngiOjAsInkiOjB9LCJjdXJyZW50WFkiOnsieCI6NzI1LCJ5Ijo3MTJ9LCJzaXplIjpbMjYsMjZdLCJjb250YWluZXJTaXplIjpbMzYsMzZdfSx7InJlZiI6bnVsbCwicmFkaXVzIjozLjY1MzA5NzU2Mzc4ODgwNSwiY29sb3VyIjp7InJnYiI6eyJyIjozMy4wNDUxMjk5ODUxOTA2NSwiZyI6MjI0LjU0NzAzNDkzOTEwMjY3LCJiIjoyNTIuNDAwNzMzOTM1Mjc4MjUsImEiOjF9LCJoc3YiOnsiaCI6MTg3LjYxODc3OTMyMzA1MjA4LCJzIjowLjg2OTA3NjcyNzgyODkxMjcsInYiOjAuOTg5ODA2Nzk5NzQ2MTg5MiwiYSI6MX0sImhzbCI6eyJoIjoxODcuNjE4Nzc5MzIzMDUyMDgsInMiOjAuOTc2ODQ5NTM3Mzk3OTU2LCJsIjowLjU1OTY5Nzc3MjM5MzA3NjMsImEiOjF9LCJoZXgiOiIjMjFlMGZjIn0sInNob3dQaWNrZXIiOmZhbHNlLCJwb2ludGVyT2Zmc2V0Ijp7IngiOjAsInkiOjB9LCJjdXJyZW50WFkiOnsieCI6MTI2MSwieSI6Mzk4fSwic2l6ZSI6WzI2LDI2XSwiY29udGFpbmVyU2l6ZSI6WzM2LDM2XX1d',
                 [],
                 'Open in Draggy'
+              )}
+            </p>
+          </>
+        }
+      ></Article>
+      <Article
+        id={'triangles'}
+        type={'2c'}
+        portrait={portrait}
+        options={{ ltr: false }}
+        img={
+          {
+            type: 'big-picture',
+            images: [
+              // [tri_1, 'Triangles and Colours'],
+              // [tri_2, 'Triangles and Colours'],
+              // [tri_3, 'Triangles and Colours'],
+              [tri_10, 'Triangles and Colours'],
+              [tri_6, 'Triangles and Colours'],
+              // [tri_9, 'Triangles and Colours'],
+              [tri_4, 'Triangles and Colours'],
+              // [tri_5, 'Triangles and Colours'],
+              [tri_8, 'Triangles and Colours'],
+              // [tri_7, 'Triangles and Colours'],
+            ],
+          }
+          //   <img
+          //     src={origami}
+          //     className="big-picture"
+          //     alt="Results of Origami algorithm that we presented in the IASS International Conference on Spatial Structures 2020/21."
+          //   />
+        }
+        heading={'Triangles!'}
+        tagline={'Delanauy Colours'}
+        text={
+          <>
+            <p>
+              Triangles is a unique coding project that creates stunning
+              gradient effects by applying Delaunay triangulation to a
+              rectangle. The system starts by randomly selecting a few key
+              points in the rectangle and assigning them vibrant colors. Then,
+              using the Delaunay algorithm, the rectangle is triangulated into a
+              beautiful mesh of triangles. The colors of these triangles are
+              calculated using bivariate spline interpolation, resulting in a
+              mesmerizing gradient effect that covers the entire rectangle.
+            </p>
+            <p>
+              To add even more visual interest to the final image, Triangles
+              uses dominant and recessive color points. The dominant points have
+              a larger radius of influence, causing the surrounding area to take
+              on their color. This creates bold, prominent spots in the gradient
+              that add depth and texture to the final image. Triangles's clever
+              use of Delaunay triangulation and bivariate spline interpolation
+              makes for an efficient and aesthetically pleasing way to generate
+              gradient effects.
+            </p>
+            <p>
+              In a future development of the project, called Draggy,
+              interpolation is replaced with Laurent polynomials, making the
+              computation even faster and more efficient. Triangles and Draggy
+              are both innovative projects that showcase the creative
+              possibilities of mathematical concepts in coding. Whether you're a
+              seasoned coder or just starting out, these projects are sure to
+              inspire and delight.
+            </p>
+            <p>
+              {makeLink(
+                'Show me!',
+                'https://triangles.ramdon.team/',
+                [],
+                'Open Triangles'
               )}
             </p>
           </>
