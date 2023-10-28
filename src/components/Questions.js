5f1859e1609713d2c8b4c71d411b61f5a35d3ad4
kichappa <kichappa@gmail.com>
2023-10-27 23:33:53 -0400

    Made questions fun

--- src/components/Questions.js
+++ src/components/Questions.js
@@ -1,11 +1,18 @@
 import React, { useEffect } from 'react';
+import { Link } from 'react-router-dom';
+import { makeLink } from './Article';
 
 const Questions = () => {
   useEffect(() => {
     document.title = 'Kishore Shenoy';
   }, []);
 
-  return <p>Here are my questions</p>;
+  return (
+    <p>
+      Shh! You were not supposed to see this. This page is under construction.{' '}<span className="tooltip-text">
+      <Link to="/">Let me show you your way out.</Link></span>
+    </p>
+  );
 };
 
 export default Questions;
